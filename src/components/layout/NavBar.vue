f6700d6ed7d86182919cb1b5f907cfbcd2bc9a6a
CSenshi <saba.pochkhua@gmail.com>
2020-08-16 00:29:13 +0400

    add header

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,23 +1,50 @@
 <template>
   <div id="nav">
-    <router-link to="/">
-      Home
-    </router-link> |
-    <router-link to="/about">
-      About
-    </router-link>
+    <nav class="navbar navbar-dark bg-dark nav-custom">
+      <div>
+        <router-link to="/">
+          Home
+        </router-link> |
+        <router-link to="/about">
+          About
+        </router-link>
+      </div>
+      <div>
+        <!-- Should Be Search Bar -->
+      </div>
+      <!-- <SignIn /> -->
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script>
+// import SignIn from './sign-in/SignIn'
+
 export default {
-  name: "NavBar"
+  name: "NavBar",
+  // components: {
+  //   SignIn
+  // }
 }
 </script>
 
 <style scoped>
  #nav {
     min-height: calc(100vh - 245px);
+    padding: 0;
   }
+
+  .nav-custom {
+    color:white;
+  }
+
+  #nav a {
+    color:white;
+  }
+
+  #nav-in{
+    display: flex;
+  }
+  
 </style>
