6ac6c3b827600d7efe465d84df4f87a7855caef1
CSenshi <saba.pochkhua@gmail.com>
2020-08-16 16:44:57 +0400

    remove main page leave only logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,18 @@
       alt="Vue logo"
       src="../assets/logo.png"
     >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Main />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
