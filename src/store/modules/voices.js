598f37f357c87bb37bc1513f221385d225f14bbd
CSenshi <saba.pochkhua@gmail.com>
2020-08-22 03:55:24 +0400

    remove dummy data

--- src/store/modules/voices.js
+++ src/store/modules/voices.js
@@ -14,22 +14,7 @@ const actions = {
 }
 
 const mutations = {
-    setVoices: (state) => (state.voices = [{
-        "path": "https://storage.googleapis.com/original-voices/levioza-origin.wav",
-        "name": "levioza"
-    }, {
-        "path": "http://soundfxcenter.com/movies/terminator/8d82b5_Terminator_Hasta_la_Vista_Baby_Sound_Effect.mp3",
-        "name": "Terminator - Hasta la Vista Baby"
-    }, {
-        "name": "Harry Potter - Expecto Patronum",
-        "path": "http://soundfxcenter.com/movies/harry-potter/8d82b5_Harry_Potter_Expecto_Patronum_Sound_Effect.mp3"
-    }, {
-        "path": "http://soundfxcenter.com/movies/300/8d82b5_This_Is_Sparta_Sound_Effect.mp3",
-        "name": "300 - This Is Sparta!"
-    }, {
-        "name": "Terminator - I'll Be Back",
-        "path": "http://soundfxcenter.com/movies/terminator/8d82b5_Terminator_I_ll_Be_Back_Sound_Effect.mp3"
-    }])
+    setVoices: (state, voices) => (state.voices = voices)
 }
 
 
