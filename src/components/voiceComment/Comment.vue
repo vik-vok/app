9daea0e5163827e34a15c146c7f409d483d81fca
CSenshi <saba.pochkhua@gmail.com>
2020-08-26 16:38:11 +0400

    add comment component

---
+++ src/components/voiceComment/Comment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ comment.ID }}
+    <br />
+    {{ comment.text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  props: {
+    comment: Object,
+  },
+};
+</script>
--- src/components/voiceComment/VoiceComments.vue
+++ src/components/voiceComment/VoiceComments.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
+    <h1>Comments</h1>
     <div v-bind:key="comment.id" v-for="comment in this.voiceComments">
-      {{ comment }}
-      <!-- <Comment :comment="comment" /> -->
+      <Comment :comment="comment" />
     </div>
   </div>
 </template>
 
 <script>
+import Comment from "./Comment";
+
 export default {
   name: "VoiceComments",
   props: {
     voiceComments: [Object],
+  },
+  components: {
+    Comment,
   },
 };
 </script>
